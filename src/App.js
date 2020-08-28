2a5c71ff40559112a72406197645232ff36001b0
fwzfwz <fawwaazrahman2002@gmail.com>
2020-08-29 01:01:08 +0700

    push to git

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import './assets/reset.css';
 import './assets/style.css';
-import Navbar from './layouts/navbar';
 import AboutPage from './pages/aboutPage';
-import HomePage from './pages/homePage';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <HomePage />
       <AboutPage />
     </div>
   );
