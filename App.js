82e91f9734eb78ed317e1a9e8608e4f5f1e598c5
Fawwaazrahman Arandhana <fawwaazrahman2002@gmail.com>
2020-09-22 16:05:09 +0700

    Allow user to choose picture from file instead of camera

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import Login from './pages/login';
 import Wrapper from './pages/wrapper';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+import AsyncStorage from '@react-native-community/async-storage';
+import executeQuery from './db';
 
 const Stack = createStackNavigator();
 
-const App = ({auth}) => {
+const checkFirstRun = async () => {
+  const isFirstRun = await AsyncStorage.getItem('isFirstRun');
+  return isFirstRun;
+};
+
+const App = ({ auth }) => {
+  React.useEffect(() => {
+    checkFirstRun().then(ret => {
+      console.log(ret);
+      if (ret == 'true') {
+        executeQuery('DROP TABLE IF EXISTS users');
+        executeQuery(
+          'CREATE TABLE users (user_id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password TEXT NOT NULL, img_url TEXT NOT NULL)'
+        );
+        executeQuery(
+          'INSERT INTO users (username, password, img_url) VALUES (?, ?, ?)',
+          [
+            'user1',
+            'pw1',
+            'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+          ]
+        );
+      } else {
+        AsyncStorage.setItem('isFirstRun', 'false');
+      }
+    });
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -15,13 +44,13 @@ const App = ({auth}) => {
           <Stack.Screen
             name="Wrapper"
             component={Wrapper}
-            options={{headerShown: false}}
+            options={{ headerShown: false }}
           />
         ) : (
           <Stack.Screen
             name="Login"
             component={Login}
-            options={{headerShown: false}}
+            options={{ headerShown: false }}
           />
         )}
       </Stack.Navigator>
@@ -29,7 +58,7 @@ const App = ({auth}) => {
   );
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   auth: state.auth,
 });
 
--- pages/addList.js
+++ pages/addList.js
@@ -1,107 +1,95 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text, TouchableOpacity, StyleSheet, Image} from 'react-native';
-import {Input, Overlay} from 'react-native-elements';
-import {RNCamera} from 'react-native-camera';
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { Input } from 'react-native-elements';
+import RNFS from 'react-native-fs';
 import executeQuery from '../db';
+import ImagePicker from 'react-native-image-picker';
 
-const AddList = ({navigation, route}) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [imgUrl, setImg] = useState('');
-  const [isVisible, setVisible] = useState(false);
+const AddList = ({ navigation, route }) => {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [imgUrl, setImg] = React.useState('');
 
-  let camera = React.useRef(null);
-
-  useEffect(() => {
+  React.useEffect(() => {
     if (route.params) {
-      const {username, password, img_url} = route.params;
+      const { username, password, img_url } = route.params;
       setUsername(username);
       setPassword(password);
       setImg(img_url);
     }
   }, [route.params]);
 
-  const takePicture = async () => {
-    if (camera) {
-      const options = {quality: 0.1, base64: true};
-      const data = await camera.takePictureAsync(options);
-      setImg(data.uri);
+  const selectImage = async () => {
+    await ImagePicker.showImagePicker(resp => {
+      resp.didCancel
+        ? false
+        : RNFS.mkdir(
+            `${RNFS.ExternalStorageDirectoryPath}/Pictures/RNProject`
+          ).then(() => {
+            RNFS.copyFile(
+              resp.path,
+              `${RNFS.ExternalStorageDirectoryPath}/Pictures/RNProject/${resp.fileName}`
+            ).then(() =>
+              setImg(
+                `file://${RNFS.ExternalStorageDirectoryPath}/Pictures/RNProject/${resp.fileName}`
+              )
+            );
+          });
+    });
+  };
+
+  const saveData = () => {
+    if (username === '' || password === '' || imgUrl === '') {
+      alert('Please Fill All Input');
+    } else {
+      if (route.params) {
+        executeQuery(
+          'UPDATE users SET username = ?, password = ?, img_url = ? WHERE user_id = ?',
+          [username, password, imgUrl, route.params.user_id]
+        );
+      } else {
+        executeQuery(
+          'INSERT INTO users (username, password, img_url) VALUES (?, ?, ?)',
+          [username, password, imgUrl]
+        );
+      }
+      navigation.navigate('HOMEPAGE');
+      setUsername('');
+      setPassword('');
+      setImg('');
     }
-    setVisible(false);
   };
 
   return (
-    <>
-      <View
-        style={{
-          margin: 15,
-        }}>
-        <Input
-          placeholder="Username"
-          value={username}
-          onChangeText={(text) => setUsername(text)}
-        />
-        <Input
-          placeholder="Password"
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-        />
-        <Image source={{uri: imgUrl}} style={styles.cameraView} />
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => setVisible(true)}>
+    <View
+      style={{
+        margin: 15,
+      }}>
+      <Input
+        placeholder="Username"
+        value={username}
+        onChangeText={text => setUsername(text)}
+      />
+      <Input
+        placeholder="Password"
+        value={password}
+        onChangeText={text => setPassword(text)}
+        secureTextEntry={true}
+      />
+      <TouchableOpacity
+        style={imgUrl === '' ? styles.button : false}
+        onPress={selectImage}>
+        {imgUrl === '' ? (
           <Text style={styles.buttonText}>Set Image</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            if (route.params) {
-              executeQuery(
-                'UPDATE users SET username = ?, password = ?, img_url = ? WHERE user_id = ?',
-                [username, password, imgUrl, route.params.user_id],
-              );
-            } else {
-              executeQuery(
-                'INSERT INTO users (username, password, img_url) VALUES (?, ?, ?)',
-                [username, password, imgUrl],
-              );
-            }
-            navigation.navigate('HOMEPAGE');
-            setUsername('');
-            setPassword('');
-            setImg('');
-          }}>
-          <Text style={styles.buttonText}>Save</Text>
-        </TouchableOpacity>
-      </View>
-      <Overlay isVisible={isVisible} onBackdropPress={() => setVisible(false)}>
-        <>
-          <RNCamera
-            ref={(ref) => {
-              camera = ref;
-            }}
-            style={styles.cameraView}
-            type={RNCamera.Constants.Type.back}
-            flashMode={RNCamera.Constants.FlashMode.on}
-            androidCameraPermissionOptions={{
-              title: 'Permission to use camera',
-              message: 'We need your permission to use your camera',
-              buttonPositive: 'Ok',
-              buttonNegative: 'Cancel',
-            }}
-            androidRecordAudioPermissionOptions={{
-              title: 'Permission to use audio recording',
-              message: 'We need your permission to use your audio',
-              buttonPositive: 'Ok',
-              buttonNegative: 'Cancel',
-            }}
-          />
-          <TouchableOpacity onPress={() => takePicture()} style={styles.button}>
-            <Text style={styles.buttonText}>Take Picture</Text>
-          </TouchableOpacity>
-        </>
-      </Overlay>
-    </>
+        ) : (
+          <Image source={{ uri: imgUrl }} style={styles.imageView} />
+        )}
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button} onPress={saveData}>
+        <Text style={styles.buttonText}>Save</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
@@ -114,12 +102,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderRadius: 1000,
   },
-  buttonText: {color: 'white'},
-  cameraView: {
-    width: 150,
-    height: 150,
-    marginHorizontal: 100,
-    marginVertical: 25,
+  buttonText: { color: 'white' },
+  imageView: {
+    width: 300,
+    height: 300,
+    marginHorizontal: 40,
+    marginVertical: 15,
   },
 });
 
